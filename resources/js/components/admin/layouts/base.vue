45c6c6b0a30c07de91086ff0548b79c92b3c449d
Dmitry Yurkovsky <sobranieinfo@yandex.ru>
2023-02-12 23:03:45 +0700

    Version 0.2 on 12 feb 2023

---
+++ resources/js/components/admin/layouts/base.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import Header from './header.vue'
+    import Sidebar from './sidebar.vue'
+</script>
+<template>
+    <Header />
+    <Sidebar />
+</template>
+<style>
+    @import './base.css';
+</style>
